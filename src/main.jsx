b18ca202a1812139e307d946108cee3bc5dea13b
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2023-04-20 15:01:46 -0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//Functional component
+function App() {
+    return (<h1>Hola Mundo!!!</h1>)
+}
+
+ReactDOM.createRoot( document.getElementById('root')).render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
