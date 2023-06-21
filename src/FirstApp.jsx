96d220bc71d805735c0251c67c2546c50749f1e3
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2023-06-21 11:29:41 -0600

    styles with CSS

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,13 +1,9 @@
 
-const getResult = (a, b) => {
-    return a + b;
-}
-
 export const FirstApp = () => {
 
   return (
     <>
-      <h1>{ getResult(10, 2) }</h1>
+      <h1>Montserrat</h1>
         {/* <h1>{ JSON.stringify( newMessage ) }</h1> */}
         <p>Soy un parrafo</p>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FirstApp } from './FirstApp';
+import './styles.css';
 //  import { HelloWorldApp } from './HelloWorldApp'
 
 ReactDOM.createRoot( document.getElementById('root')).render(
