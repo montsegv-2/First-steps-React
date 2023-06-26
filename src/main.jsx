e4d8820a4d3c43e5d2d2c033eda05f616198bf47
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2023-06-26 15:46:22 -0600

    Create CounterApp component - HW

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
+// import { FirstApp } from './FirstApp';
 import './styles.css';
-//  import { HelloWorldApp } from './HelloWorldApp'
 
 ReactDOM.createRoot( document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp />
+        <CounterApp value={10}/>
     </React.StrictMode>
 );  
