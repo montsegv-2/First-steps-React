9222aebde95f698687e7418363793f6992870dc0
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2023-07-12 14:28:01 -0600

    Click event with button

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const handleAdd = (event) => {
+  console.log('+1')
+  value = 1000;
+}
+
 export const CounterApp = ({value}) => {
+
+
   return (
     <>
         <h1>CounterApp</h1>
-        <h2>{ value }</h2>
+        <h2> { value } </h2>
+
+        <button onClick={ handleAdd }>
+            +1
+        </button>
     </>
   )
 }
